d0e484e1fd02df74279b46d6258002c84b0bc089
Vanessa Gomez <vanessa.gomez2989@gmail.com>
2024-12-27 03:05:21 +0000

    deleting objects and folders with boto3

--- delete_object.py
+++ delete_object.py
@@ -3,9 +3,9 @@ from list_objects import list_object_keys
 
 def delete_object(client, bucket, key):
     response = client.delete_object(
-    Bucket=bucket,
-    Key=key,
-)
+        Bucket=bucket,
+        Key=key,
+    )
 
     return response
 
@@ -15,16 +15,31 @@ def delete_objects(client, bucket, keys):
     response = client.delete_objects(
         Bucket=bucket,
         Delete={
+            'Objects': objects
+        }
+    )
+
+    return response
+    
+def delete_objects_non_recursive(client, bucket, keys, prefix):
+    keys = [key for key in keys if "/" not in key[len(prefix):]]
+    objects = [{'Key': key} for key in keys]
+    
+    response = client.delete_objects(
+        Bucket=bucket,
+        Delete={
             'Objects':objects
         }
-)
-
+    )
+    
     return response
 
 if __name__ == '__main__':
     bucket = 'vgomez-boto3-12242024'
     s3 = boto3.client('s3')
     
+    prefix = "folder/foldera/"
     
-    keys = list_object_keys(s3, bucket, prefix="folder")
+    keys = list_object_keys(s3, bucket, prefix=prefix)
+
     delete_objects(s3, bucket, keys)
