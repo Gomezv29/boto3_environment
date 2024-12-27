c1d8ca53088b7bce3a37331e708d3c94d868c316
Vanessa Gomez <vanessa.gomez2989@gmail.com>
2024-12-27 03:05:36 +0000

    fixed typos

--- list_objects.py
+++ list_objects.py
@@ -6,6 +6,7 @@ def filter_objects_extension(client, bucket, extension):
     for content in response["Contents"]:
         if(extension in content["Key"][-(len(extension)):]):
             keys.append(content["Key"])
+            
     return keys
     
 
@@ -13,20 +14,14 @@ def list_object_keys(client, bucket, prefix=""):
     keys = []
     response = client.list_objects_v2(Bucket=bucket, Prefix=prefix) 
     for content in response["Contents"]:
-            keys.append(content["Key"])
+        keys.append(content["Key"])
+        
     return keys
 
 if __name__ == '__main__':
     s3 = boto3.client('s3') 
 
-# for api's always use the latest version documented on the documentation pages
-
-#response = s3.list_objects_v2(Bucket="vgomez-boto3-12242024") #Prefix="folderhere") will list anything nested under the folder named
-
     response = list_object_keys(s3, "vgomez-boto3-12242024",)
 
     response = filter_objects_extension(s3, "vgomez-boto3-12242024", ".txt")
-
-#extension = ".txt"
-
     print(response)
