2afbef067aaa1304b80159fa43e3bb01fa4d3fbd
Vanessa Gomez <vanessa.gomez2989@gmail.com>
2024-12-29 18:56:18 +0000

    deleted attachment from internet gateway and vpn using boto3

---
+++ vpc_hands_on/delete_internet_gateway_attachment.py
@@ -0,0 +1,12 @@
+import boto3
+
+internet_gateway_id = 'igw-0213f07d275e74bbb'
+vpc_id = 'vpc-01143858d90013a1d'
+
+ec2 = boto3.client('ec2')
+
+ec2.detach_internet_gateway(
+    InternetGatewayId=internet_gateway_id,
+    VpcId=vpc_id,
+)
+
