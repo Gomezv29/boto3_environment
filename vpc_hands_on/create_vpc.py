38a58cee4e3c33ba791860a80ef205fb3d6be9b7
Vanessa Gomez <vanessa.gomez2989@gmail.com>
2024-12-29 18:38:48 +0000

    added order of creation and deletion

--- vpc_hands_on/create_vpc.py
+++ vpc_hands_on/create_vpc.py
@@ -5,3 +5,20 @@ ec2 = boto3.client('ec2')
 vpc = ec2.create_vpc(CidrBlock='12.0.0.0/17')
 
 print(vpc["Vpc"]["VpcId"])
+
+# Creation route
+# VPC
+#    Subnet
+#      Route Table
+#        Associating Route Table with Subnet
+#             Internet Gateway
+#                 Attaching Internet Gateway to Route Table
+
+
+# Deletion Route
+# Delete association between Internet Gateway and Route Table
+#     Internet Gateway
+#           Delete association between Route Table and Subnet
+#                Route Table
+#                    Subnet
+#                        VPC
